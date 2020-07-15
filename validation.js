195fdb7c14028ffbf482eb1e0165007977fc86d3
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2020-07-15 23:37:31 +0200

    Update validation.js with functions to check capital letters and length of input name

--- validation.js
+++ validation.js
@@ -17,22 +17,47 @@ sendForm.addEventListener('input', () => {
 
 checkForWhiteSpaces = function (name) {
     if (name.value && !name.value.trim()) {
-        console.log("Name only consists of whitespaces or is empty!")
+        console.log("Name only consists of whitespaces or is empty, good!")
     } else {
-        console.log("No only white spaces!")
+        console.log("Not only white spaces and not empty!")
     }
 }
 
 checkIfOnlyLettersAndSpace = function (name) {
-    const letters = name.value;
-    if (letters.match("[a-zA-Z\\s\'\"]+")) {
-        console.log("Letters and whitespace!")
+    if (name.value.match("[a-zA-Z\\s\'\"]+")) {
+        console.log("Letters and whitespace, good!")
     } else {
         console.log("Boooo!")
     }
 }
 
+checkIfWordStartWithCapital = function (name) {
+    let words = name.value.split(' ');
+    let counter = 0;
+    for (let i = 0; i < words.length; i++){
+        if (words[i][0] !== words[i][0].toLowerCase()) {
+            counter++;
+        }
+    }
+    if (counter === words.length) {
+        console.log("Words start wit capital, Good!")
+    } else {
+        console.log("Bad!")
+    }
+}
+
+checkLengthOfName = function (name) {
+    if(name.value.length < 25) {
+        console.log("Length is ok!")
+    }else {
+        console.log("Length is not ok!")
+    }
+}
+
 button.addEventListener('click', () => {
-    // checkForWhiteSpaces(name);
+    checkForWhiteSpaces(name);
     checkIfOnlyLettersAndSpace(name);
+    checkIfWordStartWithCapital(name);
+    checkLengthOfName(name);
 });
+
